25210606241daf9dbcd1873de58cd343e44fc2c0
lrc-zcl <369744230@qq.com>
2025-10-13 15:22:17 +0800

    模型预测

---
+++ test_demo.py
@@ -0,0 +1,18 @@
+import os
+import torch
+from torch.utils.data import DataLoader
+from models.base_model import BaseModel
+from datasets.custom_datasets import MyDatasets, MyTestDatasets
+from torch.nn import functional as F
+from tensorboardX import SummaryWriter
+
+if __name__ == "__main__":
+    excel_path = "../data/lottery_data.xlsx"
+    datasets = MyTestDatasets(excel_path)
+    model = torch.load("../model_checkpoint/model.pth")
+    test_dataloader = DataLoader(datasets, batch_size=64, shuffle=False, drop_last=True)
+    for test_x, test_y in test_dataloader:
+        model_predict = model(test_x)
+        print(f"当前的预测结果是{model_predict} ---- 正确结果是 {test_y}")
+
+    print("执行完成")
